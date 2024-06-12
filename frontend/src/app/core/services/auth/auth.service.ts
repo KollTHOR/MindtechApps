24218c309923d526e7ea7613de4fee9dad94db05
KollTHOR <abonyiaron1999@gmail.com>
2024-06-12 11:29:44 +0200

    moved navbar, aut and pokemon service

--- frontend/src/app/core/services/auth/auth.service.ts
+++ frontend/src/app/core/services/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable, tap } from 'rxjs';
-import { environment } from '../../environments/environment';
+import { environment } from '../../../environments/environment';
 import { jwtDecode } from 'jwt-decode';
 
 @Injectable({
