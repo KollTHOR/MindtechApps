e2f164b99cabcad1e3ad6c1c1b1d2428966f798d
KollTHOR <abonyiaron1999@gmail.com>
2024-06-12 10:54:37 +0200

    added local storage management and auth functions

--- frontend/src/app/core/services/auth.service.ts
+++ frontend/src/app/core/services/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable, tap } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { jwtDecode } from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root',
@@ -26,19 +27,57 @@ export class AuthService {
     localStorage.setItem('user', userName);
   }
 
-  login(loginData: any): Observable<any> {
-    return this.http
-      .post<ILoginApiData>(`${environment.baseUrl}/login`, loginData, {
-        headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
-      })
-      .pipe(
-        tap((response: ILoginApiData) => {
-          if (response.status === 202) {
-            this.router.navigate(['/shop']);
-            this.setToken(response.token);
-            this.setUserName(response.username);
-          }
-        })
-      );
+  // login(loginData: any): Observable<any> {
+  //   return this.http
+  //     .post<ILoginApiData>(`${environment.baseUrl}/login`, loginData, {
+  //       headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  //     })
+  //     .pipe(
+  //       tap((response: ILoginApiData) => {
+  //         if (response.status === 202) {
+  //           this.router.navigate(['/pokemons']);
+  //           this.setToken(response.token);
+  //           this.setUserName(response.username);
+  //         }
+  //       })
+  //     );
+  // }
+
+  clearLocalStorage(): void {
+    localStorage.clear();
+  }
+
+  logout(): void {
+    this.clearLocalStorage();
+    this.router.navigate(['']);
+  }
+
+  checkLocalStorageData(): void {
+    if (!this.getToken() || !this.getUserName()) {
+      this.logout();
+    }
+  }
+
+  monitorLocalStorageChanges(): void {
+    window.addEventListener('storage', () => {
+      this.logout();
+    });
+  }
+
+  isTokenValid(): boolean {
+    const token: string = this.getToken();
+    const tokenPayload: any = jwtDecode(token);
+    if (Date.now() > tokenPayload.exp * 1000) {
+      this.logout();
+      return false;
+    }
+    return true;
+  }
+
+  isLoggedIn(): boolean {
+    if (this.getToken()) {
+      return true;
+    }
+    return false;
   }
 }
