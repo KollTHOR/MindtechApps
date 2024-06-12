95e96c4fbe3fb2c24858dc5c57b078e06d3fbdb4
KollTHOR <abonyiaron1999@gmail.com>
2024-06-12 14:42:15 +0200

    added routes with guarding

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,20 @@ import { RegisterComponent } from './components/auth/register/register.component
 import { LoginComponent } from './components/auth/login/login.component';
 import { PokemonListComponent } from './components/pokemon/list/list.component';
 import { PokemonDetailsComponent } from './components/pokemon/details/details.component';
+import { loginAuthGuard } from './core/guards/login-auth/login-auth.guard';
 
 export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'pokemons', component: PokemonListComponent },
-  { path: 'pokemon/:id', component: PokemonDetailsComponent },
+  {
+    path: 'pokemons',
+    component: PokemonListComponent,
+    canActivate: [loginAuthGuard],
+  },
+  {
+    path: 'pokemon/:id',
+    component: PokemonDetailsComponent,
+    canActivate: [loginAuthGuard],
+  },
   { path: '', redirectTo: '/pokemons', pathMatch: 'full' },
 ];
