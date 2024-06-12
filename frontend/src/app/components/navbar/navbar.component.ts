eff13403640a34bc7245014dd3292ff9017462a6
KollTHOR <abonyiaron1999@gmail.com>
2024-06-12 14:41:07 +0200

    created navigation buttons

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../core/services/auth/auth.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatButtonModule, MatToolbarModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  private router = inject(Router);
+  private authService = inject(AuthService);
 
+  isLoggedIn = false;
+  username = '';
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.authService.isLoggedIn();
+    this.username = this.authService.getUserName();
+  }
+
+  navigateTo(path: string) {
+    this.router.navigate([path]);
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
