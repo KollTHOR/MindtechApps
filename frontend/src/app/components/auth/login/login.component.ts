1533838d2d3415edf3f8c9671b2506a33e9e015b
KollTHOR <abonyiaron1999@gmail.com>
2024-06-12 14:41:55 +0200

    added login component

--- frontend/src/app/components/auth/login/login.component.ts
+++ frontend/src/app/components/auth/login/login.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../core/services/auth/auth.service';
+import { SnackbarService } from '../../../core/services/snackbar/snackbar.service';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
+  public snackBar = inject(SnackbarService);
 
+  loginForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    const formValues = this.loginForm.value;
+    this.authService.login(formValues).subscribe({
+      next: (response) => {
+        if (response.status === 200) {
+          this.snackBar.showSuccessMessage('Sikeres bejelentkezés!', 'Close');
+        }
+      },
+    });
+  }
 }
