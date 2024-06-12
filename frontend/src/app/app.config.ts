768d1a096aa7cbc6101bc3d7c8ed3070af3d08a3
KollTHOR <abonyiaron1999@gmail.com>
2024-06-12 16:49:00 +0200

    added interceptors to httpclient

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,13 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { ErrorHandlerInterceptor } from './core/interceptors/error-handler/error-handler.interceptor';
+import { TokenInterceptor } from './core/interceptors/token-interceptor/token-interceptor.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync('noop'),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([ErrorHandlerInterceptor, TokenInterceptor])
+    ),
   ],
 };
